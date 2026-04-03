a32b077f0c06920310ec7c0aaaa32c255fcbe5e5
payal <balotra123.singh@gmail.com>
2026-04-03 12:10:54 +0530

    prisma , postgres and keyclock setup done

---
+++ apps/todo-service/prisma/migrations/20260403054212_init_todo/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
